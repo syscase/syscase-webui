65530a04448e6787126a62b7a9647260c45bbaf9
Firas Zaidan <firas@zaidan.de>
2018-04-16 03:11:24 +0200

    Fix code style

--- lib/syscase/example/optee_smc/example23.rb
+++ lib/syscase/example/optee_smc/example23.rb
@@ -6,7 +6,13 @@ class Syscase
       # OPTEE SMC Example 23
       class Example23 < self
         def describe
-          [[optee_return_rpc_cmd, addr32_shm_cookie_high, addr32_shm_cookie_low]]
+          [
+            [
+              optee_return_rpc_cmd,
+              addr32_shm_cookie_high,
+              addr32_shm_cookie_low
+            ]
+          ]
         end
       end
     end
