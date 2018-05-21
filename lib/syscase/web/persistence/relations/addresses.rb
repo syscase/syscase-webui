f829413be71b89e69f52eb62b5bb37f9fc965918
Firas Zaidan <firas@zaidan.de>
2018-05-21 23:26:37 +0200

    Add relation `Addresses`

---
+++ lib/syscase/web/persistence/relations/addresses.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Syscase
+  class Web
+    module Persistence
+      module Relations
+        # Addresses relation
+        class Addresses < ROM::Relation[:sql]
+          schema(:addresses) do
+            attribute :id, ROM::SQL::Types::Serial
+            primary_key :id
+
+            attribute :line, ROM::SQL::Types::ForeignKey(:lines)
+
+            attribute :address, ROM::SQL::Types::Int
+            attribute :code, ROM::SQL::Types::String
+
+            associations do
+              belongs_to :lines
+            end
+          end
+        end
+      end
+    end
+  end
+end
