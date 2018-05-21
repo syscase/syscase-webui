ec92cce70d267ae49d12ccb1481937a57abe4733
Firas Zaidan <firas@zaidan.de>
2018-05-21 23:24:19 +0200

    Add `Syscase::Web::Persistence::Model::PathWithAddresses`

--- lib/syscase/web.rb
+++ lib/syscase/web.rb
@@ -80,6 +80,7 @@ require 'syscase/web/persistence/model/path'
 require 'syscase/web/persistence/model/job'
 require 'syscase/web/persistence/model/line_with_addresses'
 require 'syscase/web/persistence/model/file_with_addresses'
+require 'syscase/web/persistence/model/path_with_addresses'
 
 # Pages
 require 'syscase/web/page'
---
+++ lib/syscase/web/persistence/model/path_with_addresses.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Syscase
+  class Web
+    module Persistence
+      module Model
+        # Persistence path model with nested addresses
+        class PathWithAddresses < Syscase::Web::Model::PathWithAddresses
+          extend LB::Persistence::Model::Attributes
+
+          attribute :example, Syscase::Web::Types::Strict::Int
+          attribute :index, Syscase::Web::Types::Strict::Int
+
+          redefine_attribute :addresses, Syscase::Web::Types::Strict::Array
+            .member(Syscase::Web::Persistence::Model::Address)
+        end
+      end
+    end
+  end
+end
