007be53ac6d0ede56b5d6e534f96b7f37c298c4a
Firas Zaidan <firas@zaidan.de>
2018-06-02 16:42:43 +0200

    Add `Syscase::Web::Persistence::Model::FunctionWithAddresses`

---
+++ lib/syscase/web/persistence/model/function_with_addresses.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Syscase
+  class Web
+    module Persistence
+      module Model
+        # Persistence function model with nested addresses
+        class FunctionWithAddresses < Syscase::Web::Model::FunctionWithAddresses
+          extend LB::Persistence::Model::Attributes
+
+          attribute :id, Syscase::Web::Types::Strict::Int
+
+          redefine_attribute :lines, Syscase::Web::Types::Strict::Array
+            .member(Syscase::Web::Persistence::Model::LineWithAddresses)
+        end
+      end
+    end
+  end
+end
