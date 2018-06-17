bc2f88fc89a576dd82e809c3aef4d0158267b61c
Firas Zaidan <firas@zaidan.de>
2018-06-17 18:04:28 +0200

    Refactor `Syscase::Web::GDB` to create model instances

--- lib/syscase/web/gdb.rb
+++ lib/syscase/web/gdb.rb
@@ -31,14 +31,14 @@ class Syscase
       end
 
       def disassembled_functions
-        functions.transform_values do |functions|
-          functions.map do |function|
-            function.merge(
+        functions.map do |function|
+          Syscase::Web::Model::FunctionWithAddresses.new(
+            function.to_h.merge(
               lines: Lines.new(
-                disassemble("'#{function[:file]}'::#{function[:name]}")
+                disassemble("'#{function.file.name}'::#{function.name}")
               ).call
             )
-          end
+          )
         end
       end
 
@@ -52,25 +52,25 @@ class Syscase
         end
 
         def call
-          @gdb_files.each_with_object({}) do |gdb_file, hash|
+          @gdb_files.each_with_object([]) do |gdb_file, result|
             file, functions = gdb_file.split(":\n", 2)
             file = map_file(file)
-            hash[file] = map_functions(functions.split("\n"), file)
+            result.concat(map_functions(functions.split("\n"), file))
           end
         end
 
         def map_file(file)
           file.slice!('File ')
-          file
+          Syscase::Web::Model::File.new(name: file)
         end
 
         def map_functions(functions, file)
           functions.map do |function|
-            {
+            Syscase::Web::Model::Function.new(
               name: name_for(function),
               signature: function,
               file: file
-            }
+            )
           end
         end
 
@@ -79,37 +79,44 @@ class Syscase
         end
       end
 
-      # Transform GDB disassemble result to hash
+      # Transform GDB disassemble result to array of
+      # Syscase::Web::Model::LineWithAddresses
       class Lines
         LINE_REGEX = /(^[0-9]+)([^0-9]+?.*)/
+        DUMP_REGEX = /Dump of assembler code for function .*?:\n(.*)/m
 
         def initialize(gdb_output)
-          gdb_output.slice!(
-            "Dump of assembler code for function cpu_spin_lock_dldetect:\n"
-          )
-          @gdb_blocks = gdb_output
+          @gdb_blocks = gdb_output[DUMP_REGEX, 1]
                         .split("\nEnd of assembler dump.", 2)[0]
                         .split("\n\n")
         end
 
         def call
           file = nil
-          @gdb_blocks.each_with_object({}) do |gdb_block, hash|
-            line, rest, match = next_line_match(gdb_block)
-            unless match
-              file = line[/(.*):\n/, 1]
-              gdb_block = rest
-            end
-            hash.merge!(parse_lines(gdb_block, file))
+          @gdb_blocks.each_with_object([]) do |gdb_block, result|
+            file, gdb_block = fetch_file(file, gdb_block)
+            result.concat(parse_lines(gdb_block, file))
           end
         end
 
         private
 
-        def parse_lines(block, file, lines = {})
+        def fetch_file(file, gdb_block)
+          line, rest, match = next_line_match(gdb_block)
+          unless match
+            file = Syscase::Web::Model::File.new(
+              name: line[/(.*):\n/, 1]
+            )
+            gdb_block = rest
+          end
+
+          [file, gdb_block]
+        end
+
+        def parse_lines(block, file, lines = [])
           _, rest, match = next_line_match(block)
           return map_lines(lines, parse_addresses(block)) unless match
-          parse_lines(rest, file, lines.merge(map_line(file, match)))
+          parse_lines(rest, file, lines << map_line(file, match))
         end
 
         def next_line(block)
@@ -124,29 +131,34 @@ class Syscase
 
         def map_line(file, match)
           number = Integer(match[1])
-          {
-            number => { file: file, line: number, code: match[2] }
-          }
+          Syscase::Web::Model::Line.new(
+            file: file,
+            line: number,
+            code: match[2]
+          )
         end
 
         def map_lines(lines, addresses)
-          lines.transform_values do |line|
-            line.merge(addresses: addresses)
+          lines.map do |line|
+            Syscase::Web::Model::LineWithAddresses.new(
+              line.to_h.merge(addresses: addresses)
+            )
           end
         end
 
-        def parse_addresses(block, addresses = {})
+        def parse_addresses(block, addresses = [])
           line, rest = next_line(block)
           match = /   (0x[\h]+?) <\+[0-9]+?>:([^\n]*)/.match(line)
           return addresses unless match
-          parse_addresses(rest, addresses.merge(map_address(line, match)))
+          parse_addresses(rest, addresses << map_address(line, match))
         end
 
         def map_address(_line, match)
           address = Integer(match[1])
-          {
-            address => { address: address, code: match[2] }
-          }
+          Syscase::Web::Model::Address.new(
+            address: address,
+            code: match[2]
+          )
         end
       end
     end
