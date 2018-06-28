c658b1863ac5b6e78b7c468278b8854326e734ef
Firas Zaidan <firas@zaidan.de>
2018-06-28 14:27:38 +0200

    Fix `Line` model
    * Add `id` attribute
    * Add `function` attribute

--- lib/syscase/web/persistence/model/line.rb
+++ lib/syscase/web/persistence/model/line.rb
@@ -6,6 +6,8 @@ class Syscase
       module Model
         # Persistence line model
         class Line < Syscase::Web::Model::Line
+          attribute :id, Syscase::Web::Types::Strict::Int
+          attribute :function, Syscase::Web::Types::Strict::Int
         end
       end
     end
