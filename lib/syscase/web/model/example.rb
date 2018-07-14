1d378623d9adc24adf900a1b9c9049dcd775ba7e
Firas Zaidan <firas@zaidan.de>
2018-07-14 22:30:33 +0200

    Fix code style of `Model::Example`

--- lib/syscase/web/model/example.rb
+++ lib/syscase/web/model/example.rb
@@ -5,10 +5,10 @@ class Syscase
     class Model
       # Example model
       class Example < self
-        attribute :input, Syscase::Web::Types::Strict::String
-        attribute :path, Syscase::Web::Types::Strict::String
+        attribute :input,  Syscase::Web::Types::Strict::String
         attribute :result, Syscase::Web::Types::Strict::String
         attribute :sha256, Syscase::Web::Types::Strict::String
+        attribute :path,   Syscase::Web::Types::Strict::String
         attribute :time,   Syscase::Web::Types::Strict::Time
       end
     end
