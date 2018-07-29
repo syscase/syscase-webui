ef8b4e97073f9f3c84430bfd4d6072df70d5cb12
Firas Zaidan <firas@zaidan.de>
2018-07-29 22:05:43 +0200

    Add columns `secure_log` and `normal_log` to table `examples`
    * Add schema migration to add columns `secure_log` and `normal_log` to
      table `examples`
    * Add attributes `secure_log` and `normal_log` to `Relations::Examples`
    * Add attributes `secure_log` and `normal_log` to `Model::Example`
    * Add support for importing secure and normal log files to
      `ExampleImport`
    * Add support for importing secure and normal log files to
      `Worker`
    * Add arguments `SECURE_LOG_FILE` and `NORMAL_LOG_FILE` to
      `bin/import-example`

--- lib/syscase/web/model/example.rb
+++ lib/syscase/web/model/example.rb
@@ -5,11 +5,13 @@ class Syscase
     class Model
       # Example model
       class Example < self
-        attribute :input,  Syscase::Web::Types::Strict::String
-        attribute :result, Syscase::Web::Types::Strict::String
-        attribute :sha256, Syscase::Web::Types::Strict::String
-        attribute :path,   Syscase::Web::Types::Strict::String
-        attribute :time,   Syscase::Web::Types::Strict::Time
+        attribute :input,      Syscase::Web::Types::Strict::String
+        attribute :result,     Syscase::Web::Types::Strict::String
+        attribute :sha256,     Syscase::Web::Types::Strict::String
+        attribute :path,       Syscase::Web::Types::Strict::String
+        attribute :time,       Syscase::Web::Types::Strict::Time
+        attribute :secure_log, Syscase::Web::Types::Strict::String
+        attribute :normal_log, Syscase::Web::Types::Strict::String
       end
     end
   end
