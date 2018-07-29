c78669ce696c5ebf59143291943b9ff10eb13f9c
Firas Zaidan <firas@zaidan.de>
2018-07-29 22:19:35 +0200

    Add rendering of secure and normal log to `Page::Example`

--- lib/syscase/web/page/example.rb
+++ lib/syscase/web/page/example.rb
@@ -33,6 +33,14 @@ class Syscase
                       .join("\n")
         end
 
+        def secure_log
+          Base64.decode64(example.fetch(:secure_log))
+        end
+
+        def normal_log
+          Base64.decode64(example.fetch(:normal_log))
+        end
+
         def name
           [
             format('%05d', id),
