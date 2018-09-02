4ab27444be9e77129aaf9e50d68b82858ed0b532
Firas Zaidan <firas@zaidan.de>
2018-09-02 12:10:22 +0200

    Fix `address` type to `numeric`
    PostgreSQL type `bigint` is to small for 64bit unsigned addresses.

--- lib/syscase/web/model/address.rb
+++ lib/syscase/web/model/address.rb
@@ -5,7 +5,7 @@ class Syscase
     class Model
       # Address model
       class Address < self
-        attribute :address, Syscase::Web::Types::Strict::Int
+        attribute :address, Syscase::Web::Types::Coercible::Int
         attribute :code, Syscase::Web::Types::Strict::String
       end
     end
