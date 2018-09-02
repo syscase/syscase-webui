0b4b4a5e19a8e1af9e23d61b4932e21480d1d067
Firas Zaidan <firas@zaidan.de>
2018-09-02 12:33:22 +0200

    Add coverage counts to coverage page

--- lib/syscase/web/page.rb
+++ lib/syscase/web/page.rb
@@ -5,6 +5,22 @@ class Syscase
     # Base page
     class Page < LB::Project::Page
       LB::Project::View.setup(self)
+
+      def covered
+        @covered ||= Syscase::Web.db.repository(
+          Syscase::Web::Persistence::Repositories::Paths
+        ).coverage_count
+      end
+
+      def coverage
+        @coverage ||= covered.to_f / total.to_f
+      end
+
+      def total
+        @total ||= Syscase::Web.db.repository(
+          Syscase::Web::Persistence::Repositories::Addresses
+        ).count
+      end
     end
   end
 end
